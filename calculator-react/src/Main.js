3d40500f66c425ecfad461342f0294311e85934a
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 00:45:41 +0100

    created calculator-react app

---
+++ calculator-react/src/Main.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+function Main()
+{    
+        return (
+            <div className='row justify-content-center'>
+                <div className='col-md-5'>
+                    <h1>React Calculator</h1>
+
+                    <table className='table'>
+                        <tr>
+                            <input type='text' />
+                            <button>c</button>
+                        </tr>
+
+                        <tr>
+                            <button>1</button>
+                            <button>2</button>
+                            <button>3</button>
+                            <button>/</button>
+                        </tr>
+
+                        <tr>
+                            <button>4</button>
+                            <button>5</button>
+                            <button>6</button>
+                            <button>-</button>
+                        </tr>
+
+                        <tr>
+                            <button>7</button>
+                            <button>8</button>
+                            <button>9</button>
+                            <button>+</button>
+                        </tr>
+
+                        <tr>
+                            <button>.</button>
+                            <button>0</button>
+                            <button>=</button>
+                            <button>*</button>
+                        </tr>
+
+
+                    </table>
+                </div>
+            </div>
+        )
+    }
+
+export default Main;
