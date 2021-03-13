01f2f67437924d4e7d45e94fcf7dd2bd7473aac1
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 23:58:25 +0100

    updated validation form

--- valudation-form/index.js
+++ valudation-form/index.js
@@ -3,5 +3,10 @@ function validate()
     var username = document.getElementById('username').value
     var email = document.getElementById('email').value
     var password = document.getElementById('password').value
-    var cpassword = document.getElementById('cpassword').value
+    var cpassword = document.getElementById('cpassword').
+    
+    checkusername(username)
+    checkemail(email)
+    checkpassword(password)
+    checkpasswordsmatch(password, cpassword)
 }
