e31b2c9cef8b19c0353bbbe1feb43efcf1c34ea8
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 01:40:35 +0100

    completed calculator-react app

--- calculator-react/src/Main.js
+++ calculator-react/src/Main.js
@@ -1,7 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 function Main()
 {    
+    const [inputvalue, setinputvalue]=useState('')
+
+    function display(value) 
+    {
+        setinputvalue(inputvalue + value)
+    }
+
+    function calculate()
+    {
+        var answer = eval(inputvalue)
+        setinputvalue(answer)
+    }
+
+    function clear()
+    {
+        setinputvalue('')
+    }
         return (
             <div className='row justify-content-center'>
                 <div className='col-md-5 shadow-lg p-3 mb-5 bg-white rounded'>
@@ -9,36 +26,36 @@ function Main()
 
                     <table className='table'>
                         <tr>
-                            <td colSpan='3'><input type='text'/></td>
-                            <td><button>c</button></td>
+                            <td colSpan='3'><input type="text" value={inputvalue} /></td>
+                            <td><button onClick={clear}>c</button></td>
                         </tr>
 
                         <tr>
-                            <td><button>1</button></td>
-                            <td><button>2</button></td>
-                            <td><button>3</button></td>
-                            <td><button>/</button></td>
+                            <td><button onClick={()=>{display('1')}} >1</button></td>
+                            <td><button onClick={()=>{display('2')}} >2</button></td>
+                            <td><button onClick={()=>{display('3')}} >3</button></td>
+                            <td><button onClick={()=>{display('/')}} >/</button></td>
                         </tr>
 
                         <tr>
-                            <td><button>4</button></td>
-                            <td><button>5</button></td>
-                            <td><button>6</button></td>
-                            <td><button>-</button></td>
+                            <td><button onClick={()=>{display('4')}} >4</button></td>
+                            <td><button onClick={()=>{display('5')}} >5</button></td>
+                            <td><button onClick={()=>{display('6')}} >6</button></td>
+                            <td><button onClick={()=>{display('-')}} >-</button></td>
                         </tr>
 
                         <tr>
-                            <td><button>7</button></td>
-                            <td><button>8</button></td>
-                            <td><button>9</button></td>
-                            <td><button>+</button></td>
+                            <td><button onClick={()=>{display('7')}} >7</button></td>
+                            <td><button onClick={()=>{display('8')}} >8</button></td>
+                            <td><button onClick={()=>{display('9')}} >9</button></td>
+                            <td><button onClick={()=>{display('+')}} >+</button></td>
                         </tr>
 
                         <tr>
-                            <td><button>.</button></td>
-                            <td><button>0</button></td>
-                            <td><button>=</button></td>
-                            <td><button>*</button></td>
+                            <td><button onClick={()=>{display('.')}} >.</button></td>
+                            <td><button onClick={()=>{display('0')}} >0</button></td>
+                            <td><button onClick={()=>calculate()} >=</button></td>
+                            <td><button onClick={()=>{display('*')}} >*</button></td>
                         </tr>
 
                     </table>
