960e9e8902c7b0770062c060ae3f659f85b7911b
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 23:55:13 +0100

    updated validation form

---
+++ valudation-form/index.js
@@ -0,0 +1,7 @@
+function validate()
+{
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var cpassword = document.getElementById('cpassword').value
+}
