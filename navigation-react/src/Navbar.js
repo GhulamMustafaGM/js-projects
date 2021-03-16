4477feed192c9873ee93eda0f9ff5ec9886b90be
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-16 23:43:33 +0100

    updated Navbar app

--- navigation-react/src/Navbar.js
+++ navigation-react/src/Navbar.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            <h1>Navbar component</h1>
 
             <div>
                 <button><i class="fas fa-bars"></i></button>
