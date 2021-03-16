98b6a34e25a63a6f0b47cbab6c13ac5b12d05eda
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-16 23:29:48 +0100

    create Navbar page

--- navigation-react/src/App.js
+++ navigation-react/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Navbar React app</h1>
+      <Navbar />
     </div>
   );
 }
