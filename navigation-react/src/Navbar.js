6b5553d651d90f0f88ff58b6756120350de611f6
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-16 23:33:58 +0100

    updated Navbar app

--- navigation-react/src/Navbar.js
+++ navigation-react/src/Navbar.js
@@ -4,6 +4,16 @@ export default function Navbar() {
     return (
         <div>
             <h1>Navbar component</h1>
+
+            <div>
+                <button>click</button>
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Contact us</li>
+                    <li>About</li>
+                </ul>
+            </div>
         </div>
     )
 }
