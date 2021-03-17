ed1b6717f072453d3e36b11b9d582314bc05258c
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-17 21:40:49 +0100

    added fontawesome

---
+++ todolist/index.js
@@ -0,0 +1,7 @@
+function addtolist()
+{
+    var taskname = document.getElementById('taskname').value 
+    var tododiv = document.getElementById('mytodo');
+
+    var newtodoitem = document.getElementById('')
+}
