baca07f1948cd584a9145c8b48b132de4637766f
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-17 22:10:40 +0100

    updated todolist app

--- todolist/index.js
+++ todolist/index.js
@@ -3,11 +3,18 @@ function addtolist()
     var taskname = document.getElementById('taskname').value 
     var tododiv = document.getElementById('mytodo');
 
-    var newtodoitem = document.getElementById('div')
+    var newtodoitem = document.createElement('div')
 
     var todoname = document.createElement('li')
     todoname.innerHTML = taskname
     var deletebtn = document.createElement('i')
     deletebtn.classList.add('far')
     deletebtn.classList.add('fa-trash-alt')
+
+    newtodoitem.appendChild(todoname)
+    newtodoitem.appendChild(deletebtn)
+
+    tododiv.appendChild(newtodoitem)
 }
+
+var 
