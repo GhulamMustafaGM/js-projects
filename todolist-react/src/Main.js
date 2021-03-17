2381176bd99724a0b56c6cffaf323b8031a18852
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-17 22:51:15 +0100

    updated styles app

--- todolist-react/src/Main.js
+++ todolist-react/src/Main.js
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <div>
             <div className="row justify-content-center">
-                <div className="col-md-5">
+                <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
                     <h1> Todo List React App </h1>
                         <input type="text" placeholder='Enter task' className='form-control' />
                             <button className="btn btn-success">ADD</button>
