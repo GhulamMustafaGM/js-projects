4d444ad4428da91afdf02a0f483954a1f2253982
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-20 08:04:26 +0100

    completed covid19 stats react app

--- covid19-react-app/src/Main.js
+++ covid19-react-app/src/Main.js
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'jquery/dist/jquery.min.js';
+//Datatable Module
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import $ from 'jquery';
 
 export default function Main() {
+    
+    const [coviddata, setcoviddata]=useState([])
 
     useEffect(()=> {
         axios.get('https://api.covid19api.com/summary').then(res=>{
             console.log(res.data)
+            setcoviddata(res.data.Countries)
         }).catch(err=>{console.log(err)})
 
-    }, [])
+        $(document).ready(()=>{
+            $('#mytable').DataTable()
+        })
+
+    }, [coviddata])
+
+    const tabledata = coviddata.map(obj=>{
+        return <tr>
+            <td>{obj.Country}</td>
+            <td>{obj.TotalConfirmed}</td>
+            <td>{obj.TotalConfirmed - obj.TotalRecovered}</td>
+            <td>{obj.TotalRecovered}</td>
+            <td>{obj.TotalDeaths}</td>
+        </tr>
+    })
 
     return (
         <div>
-            <h1>Covid19 Stats</h1>
-
+            <h1 className='m-5' style={{backgroundColor:'black', color:'white' }}>Covid19 Stats</h1>
             <div className="row justify-content-center">
                 <div className="col-md-8">
-                    <table className='table table-dark'>
+                    <table id="mytable" className='table table-bordered'>
                         <thead>
                             <tr>
                                 <th>Country</th>
@@ -26,6 +47,10 @@ export default function Main() {
                                 <th>Deaths</th>
                             </tr>
                         </thead>
+
+                        <tbody>
+                            {tabledata}
+                        </tbody>
                     </table>
                 </div>
             </div>
