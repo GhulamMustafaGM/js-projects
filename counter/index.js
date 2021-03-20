deb6878ceb8008df512cc871984ed960243c4d33
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-20 08:12:59 +0100

    completed counter

--- counter/index.js
+++ counter/index.js
@@ -1,15 +1,12 @@
-function increment() 
-{
+function increment() {
     var number = Number(document.getElementById('number').innerHTML)
-    document.getElementById('number').innerHTML= number + 1
+    document.getElementById('number').innerHTML = number + 1
 
 }
-    function decrement() 
-{
+function decrement() {
     var number = Number(document.getElementById('number').innerHTML)
-    document.getElementById('number').innerHTML= number - 1
+    document.getElementById('number').innerHTML = number - 1
 }
-function neutral()
-{
-    document.getElementById('number').innerHTML= 0
+function neutral() {
+    document.getElementById('number').innerHTML = 0
 }
