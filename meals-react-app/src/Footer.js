f9478c8afba27b513157dc2872b0f2d4cbde6e21
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-21 08:59:27 +0100

    updated meals app

---
+++ meals-react-app/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div>
+            <p>Designed and Developed By</p>
+            <p>Meals Online Shop</p>
+        </div>
+    )
+}
