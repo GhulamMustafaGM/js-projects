6144873495b72d7db595a41114600f46ee99ca0f
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-21 08:38:45 +0100

    updated meals app

--- meals-react-app/src/Body.js
+++ meals-react-app/src/Body.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function Body() {
+
+    useEffect(() => {
+        axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+        .then(res=>{
+            console.log(res.data);
+
+        }).catch(err=>{
+            console.log(err);
+        })
+    }, [])
+
     return (
         <div>
             <h1>This is body componet</h1>
