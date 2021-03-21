283f591dfc8f60c91ace448f81448b6a2fa4b777
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-21 07:52:46 +0100

    completed meals app

--- meals/index.js
+++ meals/index.js
@@ -7,7 +7,7 @@ $.ajax({
         {
             var newitem = `<div class='col-md-3 m-2 shdo-lg p-3 mb-5 bg-white rounded text-center'>
                 <p>${response.meals[i].strMeal}</p>
-                <img src=${response.meals[i].strMealThumb} class='img-fluid'>
+                <img src=${response.meals[i].strMealThumb} class='img-fluid' />
                 <p>${response.meals[i]}.idMead</p>
             </div>`
             $('#myitems').append(newitem)
