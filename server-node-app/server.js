11d8fdcd6237baf7a5cd6c3c9a32b18183a2eeb4
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-23 08:57:34 +0100

    updated server node app

---
+++ server-node-app/server.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('This is homepage')
+});
+
+app.get('/services', (req, res) => {
+    res.send('This services page');
+});
+
+app.listen(5000, () => {
+    console.log(`Server started on port`);
+});
+
+
+
+// npm init
+// npm i express
+// npm i nodemon 
